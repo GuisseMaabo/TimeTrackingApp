9a95fca5a426974bacbd978a47e906a6f56ce81f
GuisseMaabo <cireguisse2015@gmail.com>
2022-09-02 11:51:38 +0100

    first commit

---
+++ src/app/README.md
@@ -0,0 +1,33 @@
+# Web APP Time Tracking Items 
+
+Ceci est un projet test de gestion durée de réalisation de projet  utilisant la version Angular: 14.2.0, la librairie angular-calendar et bootstrap 5. 
+
+## Get started
+
+### Clone the repo
+
+```shell
+git clone https://github.com/bryanforbes/intern-angular
+```
+
+### Install npm packages
+
+Installez le package `npm`  décrit dans le  `package.json` et  verifiez si ça marche:
+
+```shell
+npm install
+ng serve
+```
+
+#### npm scripts
+
+Ce sont les commandes les plus utiles définies dans `package.json`:
+
+* `npm start` - exécute le compilateur TypeScript, le copieur d'actifs et un serveur en même temps, tous les trois en "watch mode".
+* `npm run build` - exécute le compilateur TypeScript, le copieur d'actifs une fois.
+
+
+
+* `npm test` - build l'application et exécute les tests internes (à la fois unitaires et fonctionnels) une fois
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModalModule,
+    CommonModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    FlatpickrModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
